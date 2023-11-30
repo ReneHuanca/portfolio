ccc6f8f8435d55fde7917728118e2aa659868a4f
Rene Huanca <renehuanca999@gmail.com>
2023-11-30 18:49:52 -0400

    config: add remarks and rehype to highlighting code and content in mdx

---
+++ next.config.mjs
@@ -0,0 +1,33 @@
+import remarkGfm from 'remark-gfm'
+import createMDX from '@next/mdx'
+import remarkToc from 'remark-toc'
+import rehypePrettyCode from 'rehype-pretty-code'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions`` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
+
+/** @type {import('rehype-pretty-code').Options} */
+const options = {
+  theme: 'material-theme-palenight',
+}
+ 
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkGfm, remarkToc],
+    rehypePlugins: [
+      [rehypePrettyCode, options],
+      rehypeSlug,
+      rehypeAutolinkHeadings,
+    ],
+  },
+})
+ 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
