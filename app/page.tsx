236da919c65f05fd682d1f4b542c7ad5e08309f4
Rene Huanca <renehuanca999@gmail.com>
2023-12-04 08:57:40 -0400

    Refactor page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import ThemeSwitcher from './ui/ThemeSwitcher'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 dark:bg-gray-900">
-      My portfolio
-
-      <ThemeSwitcher />
+    <main className="flex flex-col items-center justify-between p-24">
+      Home
     </main>
   )
 }
